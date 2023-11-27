a5031ae5f7d46e9ba2e28f1201ed35542dc105fd
VictorSilva <144695609+Sweaty001@users.noreply.github.com>
2023-11-27 10:58:00 -0300

    Update altMax.java

--- altMax.java
+++ altMax.java
@@ -5,20 +5,15 @@ import java.util.Scanner;
 public class altMax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int soma;
-        System.out.println("Digite primeiro número :");
-        int A = scanner.nextInt();
-        System.out.println("Digite segundo número :");
-        int B = scanner.nextInt();
-        System.out.println("Digite terceiro número :");
-        int C = scanner.nextInt();
+        int altura = scanner.nextInt();
+       
 
-        soma = A*B*C;
-        if (scanner.hasNextInt()) {
+        int altMax = 100;
+        if (altura < altMax) {
            
-            System.out.println("A altura maxima dos numeros é " + soma );
+            System.out.println("A altura máxima do pacote é valida" );
         } else {
-            System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+            System.out.println("Altura maior que o permitido");
         }
         scanner.close();
         
