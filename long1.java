72f176d4f8a67d8a8534d8d606129fc4d704ac2c
VictorSilva <144695609+Sweaty001@users.noreply.github.com>
2023-12-01 09:08:03 -0300

    Add files via upload

---
+++ long1.java
@@ -0,0 +1,15 @@
+package LONG;
+
+import java.util.Scanner;
+
+public class long1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long num;
+         System.out.println("Informe um número: "  );
+         num = sc.nextLong();
+
+        System.out.println("seu numero é" + num );
+
+    }
+}
